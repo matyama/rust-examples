5078097db021ff7eab0a2a83be4f6f9b543d6e75
Martin Matyášek <martin.matyasek@gmail.com>
2021-08-24 11:54:34 +0200

    Add an example on cloning stack referenced data

--- src/rc.rs
+++ src/rc.rs
@@ -11,11 +11,13 @@ impl Clone for Data {
     }
 }
 
-// Container is `Clone` because `Data` are `Clone` and so are `Box` and `Rc`
+// Container is `Clone` because `Data` are `Clone` and so are `Box`, `Rc` and references
 #[derive(Clone, Debug)]
-struct Container {
+struct Container<'a> {
     // Owned data located on the stack.
     owned: Data,
+    // Immutably shared data located on the stack that must outlive container's lifetime.
+    stack_shared: &'a Data,
     // Pointer to owned data located on the heap.
     heap_owned: Box<Data>,
     // Reference counter poining to shared data located on the heap.
@@ -29,6 +31,9 @@ mod tests {
 
     #[test]
     fn it_works() {
+        // Allocate new data on the stack
+        let stack_data = Data::default();
+
         // Allocate new reference-counted data on the heap
         let rc_data = Rc::new(Data::default());
 
@@ -36,12 +41,14 @@ mod tests {
 
         let box1 = Box::new(Container {
             owned: Data::default(),
+            stack_shared: &stack_data,
             heap_owned: Data::default().into(),
             heap_shared: rc_data.clone(),
         });
 
         let box2 = Box::new(Container {
             owned: Data::default(),
+            stack_shared: &stack_data,
             heap_owned: Data::default().into(),
             heap_shared: rc_data.clone(),
         });
@@ -53,6 +60,11 @@ mod tests {
         // Owned data are cloned because the container is behind a `Box`
         assert_eq!(clone1.owned.0, 1);
         assert_eq!(clone2.owned.0, 1);
+
+        // Data behind `&` are *not* cloned, only the pointer is trivially copied
+        assert_eq!(stack_data.0, 0);
+        assert_eq!(clone1.stack_shared.0, 0);
+        assert_eq!(clone2.stack_shared.0, 0);
 
         // Heap allocated data (behind a `Box`) are cloned (also for the same reason as above)
         assert_eq!(clone1.heap_owned.0, 1);
