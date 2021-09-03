efd37a596900e07805eba05c4bf2d4f444743b06
Martin Matyášek <martin.matyasek@gmail.com>
2021-09-03 13:30:36 +0200

    Rewrite tests in error examples to rstest

--- src/errors.rs
+++ src/errors.rs
@@ -2,8 +2,8 @@ pub fn naive_div(num: i32, d: i32) -> i32 {
     num / d
 }
 
+// Note that std already contians `i32::checked_div`
 pub fn maybe_div(num: i32, d: i32) -> Option<i32> {
-    // Note that std already contians `i32::checked_div`
     if d != 0 {
         Some(num / d)
     } else {
@@ -22,44 +22,37 @@ pub fn explained_div(num: i32, d: &str) -> Result<i32, String> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::*;
 
-    #[test]
+    #[rstest]
     #[should_panic]
     fn naive_div_by_zero() {
         // This call will panic because we divide by zero!
         naive_div(42, 0);
     }
 
-    #[test]
-    fn maybe_div_works() {
-        // In normal situations we get some result
-        assert_eq!(maybe_div(42, 2), Some(21));
-
-        // However, division by zero yields a None value.
-        // Note that this is not a 'null' pointer, rather a regular value of the `Option` type.
-        //
-        // `Option` is an example of a recoverable error. It is a *context* in which a value might
-        // be missing.
-        assert_eq!(maybe_div(42, 0), None);
+    #[rstest]
+    // In normal situations we get some result
+    #[case::some(42, 2, Some(21))]
+    // However, division by zero yields a None value.
+    // Note that this is not a 'null' pointer, rather a regular value of the `Option` type.
+    //
+    // `Option` is an example of a recoverable error. It is a *context* in which a value might
+    // be missing.
+    #[case::none(42, 0, None)]
+    fn maybe_div_works(#[case] num: i32, #[case] d: i32, #[case] expected: Option<i32>) {
+        assert_eq!(maybe_div(num, d), expected);
     }
 
-    #[test]
-    fn explained_div_works() {
-        // In standard case we get an Ok result
-        assert_eq!(explained_div(42, &"2"), Ok(21));
-
-        // In other cases (parting error or division by zero) we get an Err.
-        // Built-in `Result` is another example of a type for handling recoverable errors. It can
-        // be viewd as an `Option` with an information `Err` about the failure.
-
-        assert_eq!(
-            explained_div(42, &"0"),
-            Err("Division by zero!".to_string())
-        );
-
-        assert_eq!(
-            explained_div(42, &"abcdefg"),
-            Err("invalid digit found in string".to_string())
-        );
+    #[rstest]
+    // In standard case we get an Ok result
+    #[case::ok(42, "2", Ok(21))]
+    // In other cases (parting error or division by zero) we get an Err.
+    // Built-in `Result` is another example of a type for handling recoverable errors. It can
+    // be viewd as an `Option` with an information `Err` about the failure.
+    #[case::err_zero_div(42, "0", Err("Division by zero!"))]
+    #[case::err_parse(42, "abcdefg", Err("invalid digit found in string"))]
+    fn explained_div_works(#[case] num: i32, #[case] d: &str, #[case] expected: Result<i32, &str>) {
+        assert_eq!(explained_div(num, d), expected.map_err(String::from));
     }
 }
