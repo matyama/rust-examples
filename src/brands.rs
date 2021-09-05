049adcc098238d6c01fd479bae2b39ccc1f201e9
Martin Matyášek <martin.matyasek@gmail.com>
2021-09-05 18:40:58 +0200

    Add documentation to the branded types module

--- src/brands.rs
+++ src/brands.rs
@@ -1,14 +1,32 @@
+//! This module demonstrates *branded types* on an example of a [Vec] with *unchecked-indexing*.
+//!
+//! The concept of *unchecked-indexing* means that the API of this vector is constructed in such a
+//! way that index bounds check is performed statically at compile time. This is achieved via
+//! lifetimes and there is no cost at runtime.
+//!
+//! The example is taken from the [GhostCell paper](http://plv.mpi-sws.org/rustbelt/ghostcell/).
 use std::marker::PhantomData;
 
+/// Lifetime wrapper which makes `'id` *invariant* and has no size.
+///
+/// # Example
+/// ```
+/// use rust_examples::brands::InvariantLifetime;
+///
+/// assert_eq!(std::mem::size_of::<InvariantLifetime<'_>>(), std::mem::size_of::<()>());
+/// ```
 #[derive(Default, Clone, Copy)]
 pub struct InvariantLifetime<'id>(PhantomData<*mut &'id ()>);
 
+/// Thin wrapper for [usize] which is bound to particular instance of a [BrandedVec] via `'id` and
+/// serves as an access token to the interior values.
 #[derive(Clone, Copy)]
 pub struct BrandedIndex<'id> {
     idx: usize,
     _marker: InvariantLifetime<'id>,
 }
 
+/// Thin wrapper for [Vec] which can only be accessed via an associated [BrandedIndex]
 pub struct BrandedVec<'id, T> {
     inner: Vec<T>,
     _marker: InvariantLifetime<'id>,
@@ -16,6 +34,14 @@ pub struct BrandedVec<'id, T> {
 
 /// Public API of the [BrandedVec] as presented in the
 /// [GhostCell paper](http://plv.mpi-sws.org/rustbelt/ghostcell/).
+///
+/// # Summary
+/// This implementation defines a vector which
+///  1. has static bounds check (i.e. no additional index check in [`get`](BrandedVec::get) and
+///     [`get_mut`](BrandedVec::get_mut))
+///  1. is monotonic (i.e. can only be appened to via [`push`](BrandedVec::push))
+///  1. iterior pointers can only be accessed via associated [BrandedIndex]
+///  1. is a *zero-cost abstraction* over a [Vec]
 ///
 /// # Example 1
 /// ```
@@ -58,6 +84,7 @@ pub struct BrandedVec<'id, T> {
 ///
 /// ```
 ///
+/// # Example 3
 /// Finally, [BrandedVec] is a *zero-cost abstraction* for ordinary [Vec].
 /// ```
 /// use std::mem::size_of;
@@ -73,15 +100,20 @@ impl<'id, T> BrandedVec<'id, T> {
         })
     }
 
+    /// Appends given `value` to this [BrandedVec] and returns [BrandedIndex] of this item which is
+    /// bound to `self`. Since [BrandedVec] can only be appended to, this [BrandedIndex] is
+    /// quaranteed to *always* be within bounds.
     pub fn push(&mut self, value: T) -> BrandedIndex<'id> {
         let idx = self.inner.len();
         self.inner.push(value);
         BrandedIndex {
             idx,
-            _marker: InvariantLifetime::default(),
+            _marker: self._marker,
         }
     }
 
+    /// Method which associates (brands) given `idx` with this [BrandedVec] if it is within bounds
+    /// - i.e. performs the bounds check which `Vec::get` does.
     pub fn get_index(&self, idx: usize) -> Option<BrandedIndex<'id>> {
         if idx < self.inner.len() {
             Some(BrandedIndex {
@@ -93,11 +125,19 @@ impl<'id, T> BrandedVec<'id, T> {
         }
     }
 
+    /// Get shared reference to the interior value at given [BrandedIndex] without performing a
+    /// bounds check.
     pub fn get(&self, index: BrandedIndex<'id>) -> &T {
+        // Safety: By the construction of `BrandedIndex` and from the fact that `BrandedVec` can
+        // only be appended to (`BrandedIndex` is monotonic)
         unsafe { self.inner.get_unchecked(index.idx) }
     }
 
+    /// Get mutable reference to the interior value at given [BrandedIndex] without performing a
+    /// bounds check.
     pub fn get_mut(&mut self, index: BrandedIndex<'id>) -> &mut T {
+        // Safety: By the construction of `BrandedIndex` and from the fact that `BrandedVec` can
+        // only be appended to (`BrandedIndex` is monotonic)
         unsafe { self.inner.get_unchecked_mut(index.idx) }
     }
 }
