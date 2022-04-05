80a41df8ad417402b8d5097b019f499ed8721f2a
Martin Matyášek <martin.matyasek@gmail.com>
2022-04-05 16:53:50 +0200

    Add example implementation of an HList

--- src/dependent.rs
+++ src/dependent.rs
@@ -8,6 +8,9 @@
 //!
 //! More generally, according to the article *Why dependent types matter*:
 //! > Dependently typed programs are, by their nature, proof carrying code.
+//!
+//! Finally, example of an [HList] is taken from
+//! [hlist-rs](https://github.com/plausiblelabs/hlist-rs).
 
 #![allow(clippy::len_without_is_empty)]
 #![allow(clippy::new_without_default)]
@@ -71,9 +74,59 @@ impl<N: Nat, A: Copy> Vector<N, A> {
     /// origitnal one at compilation time!
     pub fn cons(&self, x: A) -> Vector<Succ<N>, A> {
         // This is not quite functional, but that's not the point of this example.
-        let mut xs: Vec<A> = self.0.iter().copied().collect();
+        let mut xs = self.0.to_vec();
         xs.insert(0, x);
         Vector(xs, PhantomData)
+    }
+}
+
+/// Trait representing a heterogeneous list, a.k.a [HList].
+///
+/// Contrary to an ordinary list, elements of an [HList] (resp. [HCons]) may vary in types.
+pub trait HList {
+    /// Add given element to the front of this [HList].
+    fn cons<H>(self, x: H) -> HCons<H, Self>
+    where
+        Self: Sized,
+    {
+        HCons(x, self)
+    }
+
+    /// Analogy to [`Vec::len`](Vec::len).
+    fn len(&self) -> usize;
+}
+
+/// Structure representing the null pointer at the end of each [HList].
+///
+/// Alternatively, [HNil] repreents an empty [HList].
+pub struct HNil;
+
+impl HList for HNil {
+    #[inline]
+    fn len(&self) -> usize {
+        0
+    }
+}
+
+/// Structure representing a non-empty [HList] consisting of a head and tail.
+pub struct HCons<H, T: HList>(pub H, pub T);
+
+impl<H, T: HList> HCons<H, T> {
+    #[inline]
+    pub fn head(&self) -> &H {
+        &self.0
+    }
+
+    #[inline]
+    pub fn tail(&self) -> &T {
+        &self.1
+    }
+}
+
+impl<H, T: HList> HList for HCons<H, T> {
+    #[inline]
+    fn len(&self) -> usize {
+        1 + self.tail().len()
     }
 }
 
@@ -93,5 +146,14 @@ mod tests {
         let v = Vector::<Zero, u8>::new();
         let v_prime = v.cons(42);
         assert_eq!(1, v_prime.len());
+    }
+
+    #[test]
+    fn make_hlist() {
+        let hlist = HNil;
+        assert_eq!(0, hlist.len());
+
+        let hlist = hlist.cons(1).cons("two").cons(true);
+        assert_eq!(3, hlist.len());
     }
 }
